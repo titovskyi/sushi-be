612ab88f3e322026f0505dde30e88292e582919e
titovskyi <titovskyi@gmail.com>
2019-10-12 17:17:36 +0300

    revert

--- src/index.ts
+++ src/index.ts
@@ -7,41 +7,32 @@ import * as cors from "cors";
 import routes from "./routes/index";
 import * as path from "path";
 import * as multer from "multer";
-const app = express();
 
-app.get('/', (req, res, error) => {
-    res.json({name: "Sasha"})
-});
+createConnection()
+    .then(async connection => {
+        // Create a new express application instance
+        const app = express();
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000!");
-});
+        // Call middlewares
+        app.use(cors());
+        app.use(helmet());
+        app.use(bodyParser.json());
 
-// createConnection()
-//     .then(async connection => {
-//         // Create a new express application instance
-//         const app = express();
-//         console.log(';;;;');
-//         // Call middlewares
-//         app.use(cors());
-//         app.use(helmet());
-//         app.use(bodyParser.json());
-//
-//         // Set folder for public calls
-//         app.use(express.static('./public'));
-//
-//         //Set all routes from routes folder
-//         app.use("/api", routes);
-//
-//         app.all('/*', function(req, res, next) {
-//             const indexPath = path.join(__dirname, '../');
-//             res.sendFile('index.html', { root: indexPath + '/public/' });
-//         });
-//
-//         app.listen(3000, () => {
-//             console.log("Server started on port 3000!");
-//         });
-//     })
-//     .catch(error => console.log(error));
+        // Set folder for public calls
+        app.use(express.static('./public'));
+
+        //Set all routes from routes folder
+        app.use("/api", routes);
+
+        app.all('/*', function(req, res, next) {
+            const indexPath = path.join(__dirname, '../');
+            res.sendFile('index.html', { root: indexPath + '/public/' });
+        });
+
+        app.listen(3000, () => {
+            console.log("Server started on port 3000!");
+        });
+    })
+    .catch(error => console.log(error));
 
 
