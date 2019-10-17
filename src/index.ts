01dae3c49d0bac99e6da0dd75b396b561a37c90a
titovskyi <titovskyi@gmail.com>
2019-10-17 20:56:36 +0300

    change host to 127.0.0.1

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ createConnection()
             res.sendFile('index.html', { root: indexPath + '/public/' });
         });
 
-        app.listen(3000, () => {
+        app.listen(process.env.PORT || 3000, () => {
             console.log("Server started on port 3000!");
         });
     })
